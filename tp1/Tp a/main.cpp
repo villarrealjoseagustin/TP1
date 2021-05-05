fb7fd4825347d1752bd5ccdc7a7de6f62133cf76
villarrealjoseagustin <83385892+villarrealjoseagustin@users.noreply.github.com>
2021-05-05 17:38:54 -0300

    Add files via upload

---
+++ tp1/Tp a/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{float km, nt;
+cout<<" ingrese una velocidad en km por hora:  " << endl ;
+cin >> km ;
+nt= ((km *1000)/ 3600 );
+cout << "la velocidad en m por s es de :"<< nt << endl;
+return 0 ;
+
+}
+
